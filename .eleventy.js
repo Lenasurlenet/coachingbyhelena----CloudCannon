23b91d1072e06e266a50ee2686f590ee367dfd69
Lenasurlenet <lenasurlenet@gmail.com>
2021-12-15 16:30:46 +0100

    Add markup and style to homepage. Add Header znd Footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addWatchTarget("./src/_includes/");
   eleventyConfig.addWatchTarget('./src/images/');
   eleventyConfig.addWatchTarget('./src/icons/');
   eleventyConfig.addWatchTarget('./src/css/tailwind.config.js');
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
 
-
-
+  eleventyConfig.addPassthroughCopy('./src/_includes/');
   eleventyConfig.addPassthroughCopy('./src/images/');
   eleventyConfig.addPassthroughCopy('./src/icons/');
 
