5ee4125e5d2eb040afcc17be8d3f6d96517e94f7
Lenasurlenet <lenasurlenet@gmail.com>
2021-12-15 15:47:25 +0100

    Testing netmify

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/images/');
   eleventyConfig.addWatchTarget('./src/icons/');
+  eleventyConfig.addWatchTarget('./src/css/tailwind.config.js');
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
 
 
 
   eleventyConfig.addPassthroughCopy('./src/images/');
   eleventyConfig.addPassthroughCopy('./src/icons/');
-  eleventyConfig.addPassthroughCopy('./src/css/tailwind.css');
 
   return {
     markdownTemplateEngine: "njk",
