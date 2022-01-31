23c05499bcb2810ced658c4dccb978fb6178d831
Lenasurlenet <lenasurlenet@gmail.com>
2022-01-31 15:23:24 +0100

    Corrections to footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,7 +46,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp')
   ],
 }
